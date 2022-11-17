eecb1ec1c3fe043b7d044cec1ed33fba7e90ec44
Elbin <dh3183@naver.com>
2022-11-18 01:54:47 +0900

    ➕[add]: add Project

---
+++ mottled/mottled/mottledApp.swift
@@ -0,0 +1,17 @@
+//
+//  mottledApp.swift
+//  mottled
+//
+//  Created by 한빈 on 2022/11/18.
+//
+
+import SwiftUI
+
+@main
+struct mottledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
